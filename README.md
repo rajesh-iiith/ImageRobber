fbcaa269dd0e689092803600e11d5a2e7123995b
Rajesh Kumar <rajesh.kumar@research.iiit.ac.in>
2016-11-19 14:27:52 +0530

    Adding minimum image size as a download filter

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Robs all the images from a web page, leaving the useless ones.
 
 
 
-python fetch_pics.py [url] [storage_directory]
+python fetch_pics.py [url] [storage_directory] [minimum image size in KB]
 
-python fetch_pics.py https://in.udacity.com/ udacity
+python fetch_pics.py https://in.udacity.com/ udacity 10
 
-This will create a directory called udacity in your current directory, and fetch all the images from https://in.udacity.com/ into this.
+This will create a directory called udacity in your current directory, and fetch all the images (of size >=10KB) from https://in.udacity.com/ into this.
--- fetch_pics.py
+++ fetch_pics.py
@@ -7,12 +7,12 @@ from random import randint
 
 myUrl = sys.argv[1]
 toWhere = sys.argv[2]
+min_image_size_in_KB = float(sys.argv[3])
 
 if not os.path.exists(toWhere):
     os.makedirs(toWhere)
 
 response = requests.get(myUrl)
-
 parsed_body = html.fromstring(response.text)
 
 # Get links of images
@@ -27,6 +27,12 @@ print 'Found %s images' % len(images)
 image_id  = 0
 for url in images:
 	image_id = image_id + 1
-	print url
-	location = toWhere+'/'+str(image_id)+'_'+url.split('/')[-1].split('?')[0]
-	urllib.urlretrieve(url, location)
+	image_size = len(urllib.urlopen(url).read()) / float (1024)
+
+	if (image_size >= min_image_size_in_KB):
+		print "Downloading " + url
+		location = toWhere+'/'+str(image_id)+'_'+url.split('/')[-1].split('?')[0]
+		urllib.urlretrieve(url, location)
+	else:
+		print "Discarding " + url + " [size less than" + str(min_image_size_in_KB) + " KB]"
+		
