886d797a02db7c7fa545101e04deffeb95ca9ca1
Rajesh Kumar <rajesh.kumar@research.iiit.ac.in>
2016-11-19 12:02:52 +0530

    First draft of image robber

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ImageRobber
 Robs all the images from a web page, leaving the useless ones.
+
+
+
+python fetch_pics.py [url] [storage_directory]
+
+python fetch_pics.py https://in.udacity.com/ udacity
+
+This will create a directory called udacity in your current directory, and fetch all the images from https://in.udacity.com/ into this.
---
+++ fetch_pics.py
@@ -0,0 +1,37 @@
+import requests, urlparse 
+from lxml import html  
+import os, sys  
+import urllib, re, cStringIO
+from random import randint
+
+
+myUrl = sys.argv[1]
+toWhere = sys.argv[2]
+
+if not os.path.exists(toWhere):
+    os.makedirs(toWhere)
+
+response = requests.get(myUrl)
+
+parsed_body = html.fromstring(response.text)
+
+# Get links of images
+images = parsed_body.xpath('//img/@src')  
+if not images:  
+    sys.exit("Koi image nahin mila!!")
+
+# Convert relative urls to absolute ones
+images = [urlparse.urljoin(response.url, url) for url in images]  
+print 'Found %s images' % len(images)
+
+image_id  = 0
+for url in images:
+	image_id = image_id + 1
+	print url
+	location = toWhere+'/'+str(image_id)+'_'+url.split('/')[-1].split('?')[0]
+	urllib.urlretrieve(url, location)
+	
+	
+
+
+source = urllib.urlopen(myUrl).read()
