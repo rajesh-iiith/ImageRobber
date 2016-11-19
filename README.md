592f147371f3a35ffe74153000731b5c5a8852c4
Rajesh Kumar <rajesh.kumar@research.iiit.ac.in>
2016-11-19 16:02:58 +0530

    Adding flexible command line options, updating Help

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # ImageRobber
 Robs all the images from a web page, leaving the useless ones.
 
+usage: fetch_pics.py [-h] [-d DESTINATION] [-u URLOFPAGE] [-m MINSIZE]
+
+Description of your program
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DESTINATION, --destination DESTINATION
+                        directory where images will be stored
+  -u URLOFPAGE, --urlofpage URLOFPAGE
+                        URL to the web page
+  -m MINSIZE, --minsize MINSIZE
+                        Minimum size of image in KB
 
 
-python fetch_pics.py [url] [storage_directory] [minimum image size in KB]
 
-python fetch_pics.py https://in.udacity.com/ udacity 10
+python fetch_pics.py -u https://in.udacity.com/ -d udacity -m 10
 
 This will create a directory called udacity in your current directory, and fetch all the images (of size >=10KB) from https://in.udacity.com/ into this.
--- fetch_pics.py
+++ fetch_pics.py
@@ -3,11 +3,30 @@ from lxml import html
 import os, sys  
 import urllib, re, cStringIO
 from random import randint
+import argparse
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+def parse_inputs():
+	parser = argparse.ArgumentParser(description='Description of your program')
+	parser.add_argument('-d','--destination', help='directory where images will be stored', default = os.path.join(current_dir,"input"))
+	parser.add_argument('-u','--urlofpage', help='URL to the web page', default = "empty")
+	parser.add_argument('-m','--minsize', help='Minimum size of image in KB', default = '0')
+
+	args = vars(parser.parse_args())
+
+	myUrl = args['urlofpage']
+	toWhere = args['destination']
+	min_image_size_in_KB = float (args['minsize'] )
 
 
-myUrl = sys.argv[1]
-toWhere = sys.argv[2]
-min_image_size_in_KB = float(sys.argv[3])
+	return myUrl, toWhere, min_image_size_in_KB	
+
+myUrl, toWhere, min_image_size_in_KB = parse_inputs()
+
+if myUrl == 'empty':
+		print "Enter URL of the web page"
+		sys.exit(1)
 
 if not os.path.exists(toWhere):
     os.makedirs(toWhere)
@@ -30,10 +49,10 @@ for url in set(images):
 	image_id = image_id + 1
 	image_size = len(urllib.urlopen(url).read()) / float (1024)
 
+	print image_size, min_image_size_in_KB
 	if (image_size >= min_image_size_in_KB):
 		print "Downloading " + url
 		location = toWhere+'/'+str(image_id)+'_'+url.split('/')[-1].split('?')[0]
 		urllib.urlretrieve(url, location)
 	else:
-		print "Discarding " + url + " [size less than" + str(min_image_size_in_KB) + " KB]"
-		
+		print "Discarding " + url + " [size less than" + str(min_image_size_in_KB) + " KB]"	
